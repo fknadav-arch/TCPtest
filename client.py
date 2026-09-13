33ed8fc7455a0edb4691ef71ca4c2f9df0ecd700
NadavFriedman <fk.nadav@gmail.com>
2026-09-13 10:13:08 +0300

    added multithreading for client-server conversation

--- client.py
+++ client.py
@@ -1,8 +1,21 @@
 import socket
+import threading
 
 HOST = '127.0.0.1'
 PORT= 50008
 
+def recive (sock):
+    while True:
+        dataIn = sock.recv(4096)
+        print ("in: ", dataIn.decode('utf-8'))
+
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST,PORT))
 sock.sendall(b'Hello World')
+recvTh = threading.Thread(target = recive, args= (sock,), daemon=True)
+recvTh.start()
+while True:
+    dataOut = input()
+    print("out: ", dataOut)
+    sock.sendall(dataOut.encode('utf-8'))
--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 import socket
+import threading
 
 HOST = '127.0.0.1'
 PORT= 50008
 
-global sock
-global connectedSock
+def recive (sock):
+    while True:
+        dataIn = sock.recv(4096)
+        print ("in: ", dataIn.decode('utf-8'))
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((HOST,PORT))
@@ -13,10 +16,11 @@ sock.listen(1)
 connectedSock, addr = sock.accept()
 print("client name: ", addr)
 
+recvTh = threading.Thread(target = recive, args= (connectedSock,), daemon=True)
+recvTh.start()
 
 while True: 
-    data = connectedSock.recv(4096)
-    if not data:
-            break
-    print(data)
+    dataOut = input()
+    print("out: ", dataOut)
+    connectedSock.sendall(dataOut.encode('utf-8'))
     
